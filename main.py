261aa0eebf1a6670278312684bf8f775d43a4c68
Mohammed <znaid.4002@gmail.com>
2022-09-26 00:44:26 +0300

    UPDATE

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-arr_of_number=[12, 75, 150, 180, 145, 525, 50]
-for item in arr_of_number :
-    if item %5==0 and item <=150:
-           print(item)
-    elif item>500:
-        print("Done!")
-        break
+for i in range(5):
+    print(i)
+else:
+    print("Done!")
+
+
 
 
 
